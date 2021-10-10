6951ced217fd996f55ee6661f170fbeac589754f
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2021-10-10 20:07:26 +0200

    For lib

--- main.c
+++ main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
-#include "passage_heure_d_ete_et_heure_d_hiver.h"
+#include "passage_heure_d_ete_et_heure_d_hiver/passage_heure_d_ete_et_heure_d_hiver.h"
 
 int main()
 {
